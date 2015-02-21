8c869a3015d6d354868c9fb2bed72e0def538925
Abhi Agarwal <me@abhiagarwal.com>
2015-02-21 17:27:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function generateFilePath(uploadName, uploadPath) {
 }
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", websitePath);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
