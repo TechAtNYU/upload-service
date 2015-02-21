9f41ad94375dd4314c4425e9a66019bbb69cdd65
Abhi Agarwal <me@abhiagarwal.com>
2015-02-21 22:41:19 +0000

    Updating

--- index.js
+++ index.js
@@ -15,12 +15,20 @@ function generateFilePath(uploadName, uploadPath) {
   return folderName + '_' + endFilePath + '.' + extension;
 }
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  next();
-});
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
 app.use('/uploads', express.static(__dirname + pathToUploadDirectory));
 
