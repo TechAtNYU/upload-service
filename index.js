d50a762301f0561bea4c3eccb4dc8ec943e3fc6e
Abhi Agarwal <me@abhiagarwal.com>
2015-02-21 14:23:44 -0500

    Adding Google

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-var express = require("express")
-  , app = express()
-  , formidable = require('formidable')
-  , fs   = require('fs-extra');
+'use strict';
 
-var websitePath = "http://services.tnyu.org"
-  , pathToUploadDirectory = '/uploads/';
-  
+var express = require('express');
+var app = express();
+var formidable = require('formidable');
+var fs = require('fs-extra');
+var websitePath = 'http://services.tnyu.org';
+var pathToUploadDirectory = '/uploads/';
+
 // Function to easily generate a file path
-function generateFilePath(uploadName, uploadPath){
-  var folderName = uploadName.split(".")[0];
-  var extension = uploadName.split(".")[1];
-  var endFilePath = (uploadPath.split("/")[2]).split("_")[1];
- return folderName + "_" + endFilePath + "." + extension;
+function generateFilePath(uploadName, uploadPath) {
+  var folderName = uploadName.split('.')[0];
+  var extension = uploadName.split('.')[1];
+  var endFilePath = uploadPath.split('/')[2].split('_')[1];
+  return folderName + '_' + endFilePath + '.' + extension;
 }
 
-app.use("/uploads", express.static(__dirname + pathToUploadDirectory));
+app.use('/uploads', express.static(__dirname + pathToUploadDirectory));
 
-app.post('/upload', function (req, res){
+app.post('/upload', function(req, res) {
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var endFilePath = generateFilePath(files["upload"]["name"], files["upload"]["path"])
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var endFilePath = generateFilePath(files.upload.name, files.upload.path);
     var filePath = websitePath + pathToUploadDirectory + endFilePath;
-    res.end(JSON.stringify({"originalName": files["upload"]["name"], "filePath": filePath }));
+    res.end(JSON.stringify({
+      'originalName': files.upload.name,
+      'filePath': filePath
+    }));
   });
   form.on('end', function() {
     var temporaryPath = this.openedFiles[0].path;
     var endFilePath = generateFilePath(this.openedFiles[0].name, temporaryPath);
-    fs.copy(temporaryPath, __dirname + pathToUploadDirectory + endFilePath, function(err) {
+    var pathToMove = __dirname + pathToUploadDirectory + endFilePath;
+    fs.copy(temporaryPath, pathToMove, function(err) {
       if (err) {
         console.error(err);
       }
@@ -35,14 +40,16 @@ app.post('/upload', function (req, res){
   });
 });
 
-app.get('/simple-form', function (req, res){
-  res.writeHead(200, {'Content-Type': 'text/html' });
-  var form = '<form action="/upload" enctype="multipart/form-data" method="post"><input name="upload" type="file" /><input type="submit" value="Upload" /></form>';
-  res.end(form); 
+app.get('/simple-form', function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  var form = '<form action="/upload" enctype="multipart/form-data" method="post">';
+  form += '<input name="upload" type="file" /><input type="submit" value="Upload" />';
+  form += '</form>';
+  res.end(form);
 });
 
-app.get('/', function (req, res){
-  res.end("Tech@NYU image service"); 
+app.get('/', function(req, res) {
+  res.end('Tech@NYU image service');
 });
 
 app.listen(8080);
