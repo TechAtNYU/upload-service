8abb9bb548d2343f56608b13b77ed8fdd7de55b8
Abhi Agarwal <me@abhiagarwal.com>
2015-04-14 15:29:39 -0400

    Create run-node.sh

---
+++ run-node.sh
@@ -0,0 +1,9 @@
+export RackUN=
+export RackAPI=
+export RackRegion=
+export RackContainer=
+export ImageWebsitePath=http://yourwebsite.com/
+export ServiceName=Club@University
+export BasePath=http://yourwsite.com:yourport
+
+node index.js
